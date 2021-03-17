2a42432d1854fb3271381b17cf907f0619327f06
maxiTD <maxi.torresdiaz@gmail.com>
2021-03-17 15:28:48 -0300

    credenciales actualizadas

--- includes/funciones/db_conexion.php
+++ includes/funciones/db_conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('127.0.0.1:3306', 'root', '113415', 'gdlwebcamp');
+	$conn = new mysqli('us-cdbr-east-03.cleardb.com', 'be0b28e2f31a0e', 'ed159e06', 'heroku_cb3ab3c7e6aa44f');
 
 	if($conn->connect_error) {
 		echo $error -> $conn->connect_error;
